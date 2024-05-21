d53babf937588f29ca3d91927107bf1c73698027
Gbenga Ajiboye <gbenga.aj66@gmail.com>
2024-05-21 14:06:51 +0100

    add: abis

--- scripts/constants.js
+++ scripts/constants.js
@@ -1,11 +1,11 @@
 const entryPointABI =
-  require("../artifacts/contracts/EntryPoint.sol/EntryPoint.json").abi;
+  require("./abis/EntryPoint.sol/EntryPoint.json").abi;
 const assetFactoryABI =
-  require("../artifacts/contracts/AssetFactory.sol/AssetFactory.json").abi;
+  require("./abis/AssetFactory.sol/AssetFactory.json").abi;
 const assetTokenABI =
-  require("../artifacts/contracts/mock/ModelERC20.sol/ModelERC20.json").abi;
+  require("./abis/mock/ModelERC20.sol/ModelERC20.json").abi;
 const paymentTokenABI =
-  require("../artifacts/contracts/mock/PaymentToken.sol/PaymentERC20.json").abi;
+  require("./abis/mock/PaymentToken.sol/PaymentERC20.json").abi;
 
 module.exports = {
   entryPointABI,
